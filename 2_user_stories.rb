66cd867f4216bb24221479702bee98a1476ec820
Spencer <spencer.bartonfisher@me.com>
2017-05-09 11:21:07 +0100

    init_rspec

---
+++ 2_user_stories.rb
@@ -0,0 +1,25 @@
+
+user stories
+# As a person,
+# So that I can use a bike,
+# I'd like a docking station to release a bike.
+#
+# As a person,
+# So that I can use a good bike,
+# I'd like to see if a bike is working
+
+nouns
+- bike
+- docking station
+- person
+
+verbs
+- working
+- release
+- use
+
+objects | messages
+--------------------
+person  |
+Bike    | working?
+DockingS| release_bike
